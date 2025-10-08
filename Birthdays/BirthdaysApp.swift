d61f0577a3ce423f9fbfe0406bc160161c6b5977
Sama <samaa@blink.global>
2025-10-08 10:47:06 +0300

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Samaa Soltan on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
