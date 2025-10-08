34bff6beb98d665acc3ab95b32ac46d311d280c5
Sama <samaa@blink.global>
2025-10-08 11:40:10 +0300

    Initial design

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Yomn", birthday: .now),
+        Friend(name: "Saleh", birthday: Date(timeIntervalSince1970: 0)),
+    ]
+    
+    @State private var newName: String = ""
+    @State private var newDate: Date = .now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    
+                    Text(friend.name)
+                    
+                    Spacer()
+                    
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                    
+                }
+            }
+            .navigationTitle(Text("Birthdays"))
+            .safeAreaInset(edge: .bottom) {
+                VStack {
+                    Text("New Friend")
+                        .bold()
+                    DatePicker(selection: $newDate, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        
+                        TextField("Name", text: $newName)
+                    }
+                    
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newDate)
+                        friends.append(newFriend)
+                        
+                        newName = ""
+                        newDate = .now
+                    }
+                    
+                }
+                .padding()
+                .background(.bar)
+            }
+            
+            
         }
-        .padding()
     }
 }
 
--- Birthdays/Friend.swift
+++ Birthdays/Friend.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+
+struct Friend {
+    var name: String
+    var birthday: Date
+}
